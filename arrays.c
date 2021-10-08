b98f5eb83fa3996604cf35816f08143a38300ad6
geekbeats <92048255+geekbeats@users.noreply.github.com>
2021-10-08 22:23:54 +0600

    Added a c session

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double luckynumbers[]={1.2,12,14,100,1200};
+    luckynumbers[4];
+    printf("%f", luckynumbers[1]);
+    return 0;
+}
