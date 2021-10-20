5ace733267769519516b35cb1fd81d7298d0b6f5
Nitish Kumar Gupta <nitishgupta1601@gmail.com>
2021-10-21 01:10:56 +0530

    Updated Files

---
+++ Divison_C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a;
+    float b;
+    float c;
+
+    scanf("%i%f",&a,&b);
+    c=a/b;
+    printf("%.3f",c);
+    return 0;
+}
+
